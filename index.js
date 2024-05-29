6ad058b5eae936a62e4a03d1cefe389a5852f3cd
anirudhagajbhiye <anirudhagajbhiye2@gmail.com>
2024-05-29 13:57:44 +0530

    demo 16 express js hello world to static files

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var express = require("express");
 var app = express();
-var things = require("./things.js");
-var productRouter = require("./cart.js");
-var cartRouter = require("./cart.js");
+// var things = require("./things.js");
+// var cartRouter = require("./cart.js");
+// var productRouter = require("./product.js");
 
+// app.use("/things",things);
+// app.use("/products",productRouter);
+// app.use("/cart",cartRouter);
 
-app.use("/things",things);
-app.use("/cart",cartRouter);
-app.use("/products",productRouter);
+app.use(express.static('public'));
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+app.get("/first_template", function (req, res) {
+  res.render("first_view", {
+    name: { title: "Hello", age: 32 },
+    url: "http://www.tutorialspoint.com",
+  });
+});
 
 app.listen(3000);
--- product.js
+++ product.js
@@ -5,5 +5,9 @@ router.get("/", function (req, res) {
   res.send("I am product list.");
 });
 
+router.get("/*", function (req, res) {
+  res.send("Invalid url");
+});
+
 //export this router to use in our index.js
 module.exports = router;
